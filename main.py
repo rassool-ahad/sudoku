46b1ae541ab7f307073e8316940b66725d329a49
ali <ali.vz.ali@gmail.com>
2021-05-19 14:08:54 +0430

    changes

--- main.py
+++ main.py
@@ -26,7 +26,6 @@ class SudokuTable:
 
     def __repr__(self):
         return str(self.table)
-
     def complete_table(self, n):
         for i in range(n):
             self.__generate_random_position()
@@ -83,19 +82,28 @@ class SudokuTable:
 
 class SudokuGame:
     __number = [x for x in range(1, 10)]
-    possible_numbers = {}
-
+    __solutions = []
+    exx = False
     def __init__(self, t: SudokuTable) -> None:
         super().__init__()
-        # self.table = t.table
-        self.table = np.array(
-            [6, 5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 6, 0, 0, 0, 0, 2, 0, 5, 4, 0, 0, 3, 8, 0, 0, 8, 6, 0, 0, 0, 0, 4,
-             4, 0, 0, 9, 1, 8, 0, 0, 7, 5, 0, 0, 0, 0, 4, 8, 0, 0, 2, 8, 0, 0, 3, 7, 0, 1, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0,
-             3, 7, 0, 0, 0, 0, 4, 2, 6])
-        # self.table = np.zeros(81)
+        # # self.table = t.table
+        # self.table = np.array(
+        #     [6, 5, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 7, 0, 6, 0, 0, 0, 0, 2, 0, 5, 4, 0, 0, 3, 8, 0, 0, 8, 6, 0, 0, 0, 0, 4,
+        #      4, 0, 0, 9, 1, 8, 0, 0, 7, 5, 0, 0, 0, 0, 4, 8, 0, 0, 2, 8, 0, 0, 3, 7, 0, 1, 0, 0, 0, 0, 2, 0, 1, 0, 0, 0,
+        #      3, 7, 0, 0, 0, 0, 4, 2, 6])
+        self.table = np.zeros(81)
         self.table = self.table.reshape(9, 9)
 
-    def easy_solve(self, table: np.ndarray):
+    def solve(self,table = None):
+        if table is None:
+            pass
+        else:
+            self.table=table
+        self.__solutions.clear()
+        tbl, pbl = self.__easy_solve(self.table)
+        g.__difficult_solve(tbl, pbl)
+        return self.__solutions
+    def __easy_solve(self, table: np.ndarray):
         zeros = np.where(table == 0)
         zeros_x = zeros[0]
         zeros_y = zeros[1]
@@ -115,40 +123,38 @@ class SudokuGame:
                 # print(np.where(slice_area == number)[0])
                 if list(np.where(slice_area == number)[0]) == [] and list(
                         np.where(slice_row == number)[0]) == [] and list(
-                    np.where(slice_column == number)[0]) == []:
+                        np.where(slice_column == number)[0]) == []:
                     choosable_numbers.append(number)
             if len(choosable_numbers) == 0:
                 return 1
             if len(choosable_numbers) == 1:
                 table[zeros_x[i], zeros_y[i]] = choosable_numbers[0]
-                return self.easy_solve(table)
+                return self.__easy_solve(table)
             else:
                 possible_numbers[tuple([zeros_x[i], zeros_y[i]])] = choosable_numbers
         return table, possible_numbers
 
-    def difficult_solve(self, table: np.ndarray, possible_numbers: dict):
-        print(type(possible_numbers))
-
+    def __difficult_solve(self, table: np.ndarray, possible_numbers: dict):
         for position, choosable_numbers in possible_numbers.items():
             for num in choosable_numbers:
+                if self.exx:
+                    return
                 temp_table = table.copy()
                 temp_table[position[0], position[1]] = num
-                res = self.easy_solve(temp_table)
+                res = self.__easy_solve(temp_table)
                 if type(res) == tuple:
-
                     t, pbl = res
                     if len(pbl) == 0:
-                        yield t
-                        break
-                    self.difficult_solve(t, pbl)
+                        exist = False
+                        for sls in self.__solutions:
+                            if (t == sls).all():
+                                exist = True
+                        if not exist:
+                            self.__solutions.append(t)
+                            self.exx=True
+                    else:
+                        self.__difficult_solve(t, pbl)
 
-            # slice_column: np.ndarray = self.table[position[0]]
-            # slice_row: np.ndarray = self.table[:, position[1]]
-            # remain_x = position[0] - (position[0] % 3)
-            # remain_y = position[1] - (position[1] % 3)
-            # slice_area: np.ndarray = self.table[remain_x: remain_x + 3, remain_y: remain_y + 3]
-
-        pass
         # TODO
         #     rnd = np.random.choice(self.__number)
         # if list(np.where(slice_area == rnd)[0]) == [] and list(np.where(slice_row == rnd)[0]) == [] and list(
@@ -167,8 +173,8 @@ def try_many_times():
             s = SudokuTable("easy")
             print(s)
             g = SudokuGame(s)
-            a, z = g.easy_solve(g.table)
-            g.difficult_solve(a, z)
+            a, z = g.__easy_solve(g.table)
+            g.__difficult_solve(a, z)
 
             counter += 1
         print(a)
@@ -181,25 +187,8 @@ def try_many_times():
 
 # try_many_times()
 g = SudokuGame(None)
-a, z = g.easy_solve(g.table)
-# print(z)
-# print(a)
-print("____________________________________")
-# cnt = 0
-# for i in g.difficult_solve(a, z):
-#     print(i)
-#     cnt +=1
-# print(cnt)
-# # print(g.difficult_solve(a, z))
-ls = [x for x in g.difficult_solve(a, z)]
-cn = 0
-for ip in ls:
-    for itm in ls:
-        if not (ip == itm).all():
-            cn += 1
-
-print(cn)
-print(len(ls))
+z = g.solve()
+print(z)
 # numbers = [1, 2, 3, 4, 5, 6, 7]
 # for number in numbers:
 #     print(number)
